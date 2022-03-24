c0fc19e6e90cdd496b4b65825b8999f6180cbc4f
Benroberts37 <benrobsemail@gmail.com>
2022-03-24 08:32:14 -0600

    setting up application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import './App.css';
+import GifList from './components/GifList'
+import GifForm from './components/GifForm'
+import data from './data/gif'
 
-function App() {
+function App(props) {
+  const gifs = data
+  const loading = false
+  const error = ""
+
   return (
     <div className="App">
-      Async Redux Project
+      <h1>Search for gifs</h1>
+      <GifForm/>
+
+      {
+        loading ? <h3>We are loading</h3> : <GifList gifs = {gifs}/>
+      }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createStore } from 'redux'
+import { Proivder} from 'react-redux'
+
 import App from './App';
 import './index.css';
+
+const store = createStore 
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+
+
+
+// API key - UWm6GLQYKmmLnfzjn8W12UrgD2jp43un
